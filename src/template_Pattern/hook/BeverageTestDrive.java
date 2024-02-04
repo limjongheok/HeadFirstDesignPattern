60fa5df2f0b2c37c8083c745245948e18f5947fe
limjonghyeok <jjognbbang2@naver.com>
2024-02-04 10:03:15 +0900

    템플릿메소드 패턴

---
+++ src/template_Pattern/hook/BeverageTestDrive.java
@@ -0,0 +1,10 @@
+package template_Pattern.hook;
+
+public class BeverageTestDrive {
+    public static void main(String[] args){
+        CoffeeWithHook coffeeWithHook = new CoffeeWithHook();
+        System.out.println("커피 중비중");
+        coffeeWithHook.prepareRecipe();
+    }
+
+}
